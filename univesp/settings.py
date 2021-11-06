28528cb78486657189427e1d6ed5de765c15865c
160261Jorge <53455664+160261Jorge@users.noreply.github.com>
2021-11-06 18:51:51 -0300

    arrumando bd

--- univesp/settings.py
+++ univesp/settings.py
@@ -83,24 +83,12 @@ WSGI_APPLICATION = 'univesp.wsgi.application'
 
 
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'integrador',
-
-        'USER': 'postgres',
-
-        'PASSWORD': '12345678',
-
-        'HOST': 'localhost',
-
-        'PORT': '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
     }
-
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
