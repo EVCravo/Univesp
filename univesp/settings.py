594e0ba560c829ee387f5d9e73e46f64107e3e46
George_Pagliuso <georgepagliuso89@gmail.com>
2021-11-24 10:41:14 -0300

    alterações para Heroku

--- univesp/settings.py
+++ univesp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import os
 import psycopg2
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -133,3 +133,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ] 
 
+django_heroku.settings(locals())
